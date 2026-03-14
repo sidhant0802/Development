0bd1fa8fc323415307a415b8d2f03888d3a024b2
sidhant <sidhantnirupam@gmail.com>
2026-03-14 11:08:56 +0530

    your message

---
+++ CSS/1/script.js
@@ -0,0 +1,13 @@
+const demo = document.getElementById("demo");
+
+function addInline(){
+ demo.style.color = "red";
+}
+
+function removeInline(){
+ demo.style.color = "";
+}
+
+function toggleClass(){
+ demo.classList.toggle("internal");
+}
